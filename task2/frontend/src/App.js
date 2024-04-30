926f0c95513cd6431c74bc682c1f7dd03d1ced62
xclamation <100029049+xclamation@users.noreply.github.com>
2024-04-30 16:50:43 +0300

    Add files via upload

---
+++ task2/frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                
+                <p>
+                    Edit <code>src.App.js</code> and save to reload
+                </p>
+                <a className='App-link' href='/api' target='_blank' rel='noopener noreferrer'>
+                    Call the backend
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
